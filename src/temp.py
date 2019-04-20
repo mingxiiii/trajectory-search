843ccc07dd65945a3ecd66c4a3d5b5083da71421
mingxiiii <daimingxi_hazel@yahoo.com>
2019-04-20 22:10:07 +0800

    added notebooks

--- src/temp.py
+++ src/temp.py
@@ -1,7 +1,9 @@
-from rtree.index import Rtree
+# from rtree.index import Rtree
 from src.features.build_bbox import *
 
 train = 'gps_20161002_trajectory'
 rtree_id_dict_path = './data/interim/%s/rtree_id_dict.txt' % train
+print(rtree_id_dict_path)
 rtree_id_dict = read_pickle(rtree_id_dict_path)
+# print(rtree_id_dict.keys())
 save_pickle(rtree_id_dict, rtree_id_dict_path)
